da2a833086ce0a811a6c0344ea90904850238321
forezp <124746406@qq.com>
2024-03-23 16:06:46 +0000

    Deploying to gh-pages from @ forezp/JavaJike@208581822f9e1af03fad50dacb27c03ea9a56166 🚀

---
+++ assets/2023-11-8-proxy.html-19a4b13e.js
@@ -0,0 +1,124 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as i,c as n,f as l}from"./app-6bf229cf.js";const a={},s=l(`<p>代理模式和装饰器模式类似，都是在不改变同一个接口功能的前提下，对原有功能的做扩展或者增强。代理模式并没有做类似于装饰器模式多层嵌套，而是采用灵活的单一结构。在Java语言中并支持动态代理，在很多RPC框架、Spring AOP、Spring事务等领域有着广泛的应用。</p><p>代理模式是一种结构性模式，它允许将对象的访问控制和代码运行位置转移到代理对象中。类似于中介，代理对象可以控制客户端对真实对象的访问。代理模式常用于对已有功能的增强，比如访问控制、远程调用。</p><h2 id="静态代理" tabindex="-1"><a class="header-anchor" href="#静态代理" aria-hidden="true">#</a> 静态代理</h2><p>代理模式分为静态代理和动态代码，一般静态代理使用的比较少，而动态代理在各种框架、中间件有着广泛的应用。</p><p>静态代理需要手动创建一个代理类，实现被代理对象的接口，并将实际对象的方法调用转发给它。静态代理的优点是简单易懂，但缺点是需要手动创建代理类，对于需要代理的类数量较多或变化频繁的情况下，代码会变得臃肿难以维护。</p><img src="https://static.javajike.com/img/2023/11/9/image-20231109225529976.png" alt="image-20231109225529976" style="zoom:50%;"><p>从上面的 UML 图中，我们可以看出代理模式有三个关键角色：</p><ul><li><p>抽象主题接口类（Subject）：它定义了一些方法。</p></li><li><p>主题实现类（RealSubject）：实现了抽象接口类（的所有方法</p></li><li><p>代理类（StaticProxy）：实现了抽象主题类的方法，并隐藏在代理后面可能其他类的实现。</p></li></ul><h3 id="代码实现" tabindex="-1"><a class="header-anchor" href="#代码实现" aria-hidden="true">#</a> 代码实现</h3><p>定义一个抽象主题类：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>public interface Subject {
+    void operation();
+}
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>主题实现类（RealSubject）的代码如下：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>public class RealSubject implements Subject{
+    @Override
+    public void operation() {
+        System.out.println(&quot;do somthing&quot;);
+    }
+}
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>代理类（StaticProxy）实现了抽象主题类，并持有主题实现类的对象，并在主题实现类的对象的operation()方法之前和之后做了功能的增强，具体代码如下：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>public class StaticProxy implements Subject{
+
+    private RealSubject realSubject;
+
+    public StaticProxy(RealSubject realSubject) {
+        this.realSubject = realSubject;
+    }
+
+    @Override
+    public void operation() {
+        System.out.println(&quot;before operation...&quot;);
+        realSubject.operation();
+        System.out.println(&quot;after operation...&quot;);
+    }
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>写一个客户端实现类，代码如下：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>public class Client {
+    public static void main(String[] args) {
+        testStatic();
+      
+    }
+
+    public static void testStatic() {
+        StaticProxy staticProxy = new StaticProxy(new RealSubject());
+        staticProxy.operation();
+    }
+ }
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>运行代码，输出如下：</p><blockquote><p>before operation...<br> do somthing<br> after operation...</p></blockquote><h2 id="动态代理" tabindex="-1"><a class="header-anchor" href="#动态代理" aria-hidden="true">#</a> 动态代理</h2><p>动态代理可以使用 Java 动态代理机制和CGLIB动态代理。</p><h3 id="java-动态代理" tabindex="-1"><a class="header-anchor" href="#java-动态代理" aria-hidden="true">#</a> Java 动态代理</h3><p>Java 动态代理是在运行时自动生成代理类并将方法调用转发到实际对象。 Java 动态代理的优点是避免了手动创建代理类的麻烦，但缺点是对于一些无法实现接口的类，无法使用动态代理。</p><p>Java 动态代理是使用Java的反射机制来实现动态代理。Java提供了java.lang.reflect.Proxy类和java.lang.reflect.InvocationHandler接口来实现动态代理。</p><p>具体使用Java 动态代理的步骤是先实现InvocationHandler接口。和静态代理类似：在这个接口中，需要对真实的代理对象的功能需要做一下加强，它是实现动态代理的关键，代码如下：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>public class ProxyHandler implements InvocationHandler {
+
+    private Object object;
+
+    public ProxyHandler(Object object){
+        this.object = object;
+    }
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println(&quot;Before invoke &quot;  + method.getName());
+        method.invoke(object, args);
+        System.out.println(&quot;After invoke &quot; + method.getName());
+        return null;
+    }
+}
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>实现代理如下：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>public class Client {
+
+    /**
+     * jdk动态代理
+     */
+    public static void testJdkProxy() {
+        Subject realSubject = new RealSubject();
+        ProxyHandler handler = new ProxyHandler(realSubject);
+        Subject subject = (Subject) Proxy.newProxyInstance(handler.getClass().getClassLoader(),
+                realSubject.getClass().getInterfaces(), handler);
+        subject.operation();
+    }
+    
+     public static void main(String[] args) {
+        testJdkProxy();
+      
+    }
+}
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>运行代码输出：</p><blockquote><p>Before invoke operation<br> do somthing<br> After invoke operation</p></blockquote><p>Java动态代理是基于反射生产的一个代理类，这个代理类本身已经继承了jdk包中的Proxy对象，而Java是不允许多继承的，所以只能实现接口的方式进行代理。</p><p>它的优点如下：</p><ul><li>Java动态代理不需要任何依赖</li><li>灵活性：动态代理可以在运行时动态生成代理类，</li><li>可拓展性：可以通过动态代理实现一些横切关注点，比如日志记录、性能监控等，而无需修改原有的代码。</li></ul><p>缺点：</p><ul><li>性能开销：由于动态代理是在运行时动态生成代理类，相比直接调用实现类的方法，会带来一定的性能开销</li><li>功能限制：动态代理只能对接口进行代理，在某些情况下，无法代理实现类的方法。</li></ul><h3 id="cglib代码" tabindex="-1"><a class="header-anchor" href="#cglib代码" aria-hidden="true">#</a> CGLIB代码</h3><p>在pom文件中引入cglib包，代码如下：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code> &lt;dependency&gt;
+      &lt;groupId&gt;cglib&lt;/groupId&gt;
+      &lt;artifactId&gt;cglib&lt;/artifactId&gt;
+      version&gt;3.2.5&lt;/version&gt;
+&lt;/dependency&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>CGlib代码模式需要实现MethodInterceptor，并在intercept方法中实现真实对象的功能增强，代码如下：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>public class CglibProxyFactory implements MethodInterceptor {
+
+    private Object target;//维护一个目标对象
+
+    public CglibProxyFactory(Object target) {
+        this.target = target;
+    }
+
+    //为目标对象生成代理对象
+    public Object getProxyInstance() {
+        //工具类
+        Enhancer en = new Enhancer();
+        //设置父类
+        en.setSuperclass(target.getClass());
+        //设置回调函数
+        en.setCallback(this);
+        //创建子类对象代理
+        return en.create();
+    }
+
+    @Override
+    public Object intercept(Object o, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
+        System.out.println(&quot;开始cglib拦截&quot;);
+        // 执行目标对象的方法
+        Object returnValue = method.invoke(target, args);
+        System.out.println(&quot;结束cglib拦截&quot;);
+        return returnValue;
+    }
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>写一个测试类：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>public class Client {
+
+
+    public static void main(String[] args) {
+        testCglibProxy();
+    }
+
+    /**
+     * 测试cglib
+     */
+    public static void testCglibProxy() {
+        Subject subject = new RealSubject();
+        Subject proxy = (Subject) new CglibProxyFactory(subject).getProxyInstance();
+        proxy.operation();
+    }
+}
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>执行结果如下：</p><blockquote><p>开始cglib拦截<br> do somthing<br> 结束cglib拦截</p></blockquote><p>CGLIB（Code Generation Library）是一个强大的高性能的代码生成库，用于在运行时扩展Java类和实现动态代理。与Java动态代理不同，CGLIB可以代理非接口类型的类。</p><p>CGLIB动态代理的优点:</p><ul><li>性能高：相比Java动态代理，CGLIB动态代理通常能够提供更好的性能，因为它是通过生成子类来代理目标类，而不是通过实现接口。</li><li>功能强大：CGLIB能够代理普通类和接口类，更灵活地满足额外需求，例如代理私有方法、拦截静态方法等。</li><li>无需依赖接口：CGLIB动态代理可以代理没有实现任何接口的类，这使得它可以代理更多类型的类，提供更大的灵活性。</li></ul><p>CGLIB动态代理的缺点:</p><ul><li>需要额外依赖：CGLIB动态代理需要引入额外的库，增加项目的依赖，相比Java动态代理而言更为复杂。</li><li>对final方法和final类的限制：CGLIB无法代理final方法和final类。</li><li>不支持自身方法调用：CGLIB无法从代理对象中调用自身的方法，这可能会引起无限循环或抛出异常。</li></ul><figure><img src="https://static-1254191423.cos.ap-shanghai.myqcloud.com/img/common/wxcode.png" alt="方志朋_官方公众号" tabindex="0" loading="lazy"><figcaption>方志朋_官方公众号</figcaption></figure>`,50),d=[s];function t(r,c){return i(),n("div",null,d)}const b=e(a,[["render",t],["__file","2023-11-8-proxy.html.vue"]]);export{b as default};
---
+++ assets/2023-12-09-volatile.html-2931cf62.js
@@ -0,0 +1,79 @@
+import{_ as a}from"./plugin-vue_export-helper-c27b6911.js";import{r as l,o as d,c as s,a as e,b as i,d as r,f as t}from"./app-6bf229cf.js";const v={},c=t(`<p>在并发编程中，可见性、原子性和有序性的问题常常让我们感到困惑，因为这些问题容易成为 Bug 的源头。早在诞生之初，Java就在多线程方面处于领先地位，并提供了针对这些问题的技术方案。</p><p>今天先聊聊Java如何解决多线程下解决可见性和有序性问题。在此之前，先聊聊Java内存模型。</p><h2 id="java内存模型" tabindex="-1"><a class="header-anchor" href="#java内存模型" aria-hidden="true">#</a> Java内存模型</h2><p>随着计算机CPU运算能力的迅速增长，CPU已经远远超越了主内存读取数据的速度。为了解决这个问题，CPU厂商引入了高速缓存，使得CPU能够直接从高速缓存中读取数据，从而达到更高的性能。然而，在多核CPU中，每个处理器都有自己的缓存，这引发了数据一致性的问题。为了确保多核处理器的数据一致性，需要引入多处理器数据一致性协议，例如MOSI、Synapse、Firely、DragonProtocol等。这些协议帮助确保处理器之间的数据一致性，以避免出现意外的错误和不一致的状态。</p><figure><img src="https://static-1254191423.cos.ap-shanghai.myqcloud.com/img/2023/12/9/2279594-98d10d5cb7acf7a1.png" alt="JMM内存模型.png" tabindex="0" loading="lazy"><figcaption>JMM内存模型.png</figcaption></figure><p>在JVM执行多线程任务时，共享数据存储在主内存中，而每个线程（在不同的处理器上执行）都拥有自己的高速缓存。当线程对共享数据进行修改时，首先会将数据从主内存复制到线程的高速缓存中，进行修改操作后再将结果从高速缓存复制回主内存。然而，当多个线程同时进行这样的操作时，可能会导致共享数据出现意外的错误。</p><p>举个例子，比如执行i++操作，线程首先从主内存读取i的值，比如i=0，然后复制到自己的高速缓存区，进行i++操作，最后将结果再次从高速缓存区复制回主内存。如果有两个线程同时对i进行i++操作，预期的结果应该是2。然而实际上可能并非如此。线程1读取主内存的i=0后，线程2也读取i=0进行i++操作，最终导致结果为1而不是2。</p><p>为了解决缓存不一致的问题，有两种解决方案：</p><ol><li>在总线上加锁，即只允许一个线程执行对共享数据的操作（包括读取和修改）。</li><li>使用缓存一致性协议，如Intel的MESI协议。该协议的核心思想是，当某个处理器写入变量数据时，如果其他处理器也包含该变量，会发出信号通知其他处理器将高速缓存中的该数据设置为无效状态。这样其他处理器在需要读取该变量时，会重新从主内存中读取，然后再次复制到高速缓存区。</li></ol><p>这样就可以确保多线程在共享数据时不会出现意外的错误。</p><h2 id="编发编程的概念" tabindex="-1"><a class="header-anchor" href="#编发编程的概念" aria-hidden="true">#</a> 编发编程的概念</h2><p>再简单回顾一些上一篇文章讲解的并发编程的有三个概念，包括原子性、可见性、有序性。</p><h3 id="原子性" tabindex="-1"><a class="header-anchor" href="#原子性" aria-hidden="true">#</a> 原子性</h3><p>原子性是指，操作为原子性的，要么成功，要么失败，不存在第三种情况。比如：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>String s=&quot;abc&quot;;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><p>这个复杂操作是原子性的。再比如：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>int i=0;
+i++;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><p>i=0这是一个赋值操作，这一步是原子性操作；那么i++是原子性操作吗？当然不是，首先它需要读取i=0，然后需要执行运算，写入i的新值1，它包含了读取和写入两个步骤，所以不是原子性操作。</p><h3 id="可见性" tabindex="-1"><a class="header-anchor" href="#可见性" aria-hidden="true">#</a> 可见性</h3><p>可见性是指共享数据的时候，一个线程修改了数据，其他线程知道数据被修改，会重新读取最新的主存的数据。 举个例子：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>i=0;//主内存
+
+i++;//线程1
+
+j=i;//线程2
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>线程1修改了i值，但是没有将i值复制到主内存中，线程2读取i的值，并将i的值赋值给j,我们期望j=1,但是由于线程1修改了，没有来得及复制到主内存中，线程2读取了i,并赋值给j，这时j的值为0。 也就是线程i值被修改，其他线程并不知道。</p><h3 id="有序性" tabindex="-1"><a class="header-anchor" href="#有序性" aria-hidden="true">#</a> 有序性</h3><p>是指代码执行的有序性，因为代码有可能发生指令重排序（Instruction Reorder）。</p><p>Java 语言提供了 volatile 和 synchronized 两个关键字来线程代码操作的有序性，volatile 是因为其本身包含“禁止指令重排序”的语义，synchronized 在单线程中执行代码，无论指令是否重排，最终的执行结果是一致的。</p><h2 id="volatile详解" tabindex="-1"><a class="header-anchor" href="#volatile详解" aria-hidden="true">#</a> volatile详解</h2><h3 id="volatile关键字作用" tabindex="-1"><a class="header-anchor" href="#volatile关键字作用" aria-hidden="true">#</a> volatile关键字作用</h3><p>被volatile关键字修饰变量，起到了2个作用：</p><blockquote><p>1.某个线程修改了被volatile关键字修饰变量是，根据数据一致性的协议，通过信号量，更改其他线程的高速缓存中volatile关键字修饰变量状态为无效状态，其他线程如果需要重写读取该变量会再次从主内存中读取，而不是读取自己的高速缓存中的。</p><p>2.被volatile关键字修饰变量不会指令重排序。</p></blockquote><h3 id="volatile能够保证可见性和防止指令重排" tabindex="-1"><a class="header-anchor" href="#volatile能够保证可见性和防止指令重排" aria-hidden="true">#</a> volatile能够保证可见性和防止指令重排</h3><p>在Java并发编程实战一书中有这样</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>public class NoVisibility {
+    private static boolean ready;
+    private static int a;
+
+    public static void main(String[] args) throws InterruptedException {
+        new ReadThread().start();
+        Thread.sleep(100);
+        a = 32;
+        ready = true;
+      
+
+    }
+
+    private static class ReadThread extends Thread {
+        @Override
+        public void run() {
+            while (!ready) {
+                Thread.yield();
+            }
+            System.out.println(a);
+        }
+    }
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>在上述代码中，有可能（概率非常小，但是有这种可能性）永远不会打印a的值，因为线程ReadThread读取了主内存的ready为false,主线程虽然更新了ready，但是ReadThread的高速缓存中并没有更新。 另外：</p><blockquote><p>a = 32;</p><p>ready = true;</p></blockquote><p>这两行代码有可能发生指令重排。也就是可以打印出a的值为0。</p><p>如果在变量加上volatile关键字，可以防止上述两种不正常的情况的发生。</p><h3 id="volatile不能保证原子性" tabindex="-1"><a class="header-anchor" href="#volatile不能保证原子性" aria-hidden="true">#</a> volatile不能保证原子性</h3><p>首先用一段代码测试下，开起了10个线程，这10个线程共享一个变量inc（被volatile修饰），并在每个线程循环1000次对inc进行inc++操作。我们预期的结果是10000.</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>public class VolatileTest {
+
+
+    public volatile int inc = 0;
+
+    public void increase() {
+        inc++;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        final VolatileTest test = new VolatileTest();
+        for (int i = 0; i &lt; 10; i++) {
+            new Thread(() -&gt; {
+                for (int j = 0; j &lt; 1000; j++)
+                    test.increase();
+            }).start();
+        }
+        //保证前面的线程都执行完
+        Thread.sleep(3000);
+        System.out.println(test.inc);
+    }
+
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>多次运行main函数，你会发现结果永远都不会为10000，都是小于10000。可能有这样的疑问，volatile保证了共享数据的可见性，线程1修改了inc变量线程2会重新从主内存中重新读，这样就能保证inc++的正确性了啊，可为什么没有得到我们预期的结果呢？</p><p>在之前已经讲述过inc++这样的操作不是一个原子性操作，它分为读、加加、写。一种情况，当线程1读取了inc的值，还没有修改，线程2也读取了，线程1修改完了，通知线程2将线程的缓存的 inc的值无效需要重读，可这时它不需要读取inc ，它仍执行写操作，然后赋值给主线程，这时数据就会出现问题。</p><p>所以volatile不能保证原子性 。这时需要用锁来保证,在increase方法加上synchronized，重新运行打印的结果为10000 。</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code> public synchronized void increase() {
+        inc++;
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="volatile的使用场景" tabindex="-1"><a class="header-anchor" href="#volatile的使用场景" aria-hidden="true">#</a> volatile的使用场景</h3><h4 id="状态标记" tabindex="-1"><a class="header-anchor" href="#状态标记" aria-hidden="true">#</a> 状态标记</h4><p>volatile最常见的使用场景是状态标记，如下：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>private volatile boolean asheep ;
+
+//线程1
+ 
+while(!asleep){
+    countSheep();
+}
+
+//线程2
+asheep=true;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h4 id="防止指令重排" tabindex="-1"><a class="header-anchor" href="#防止指令重排" aria-hidden="true">#</a> 防止指令重排</h4><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>volatile boolean inited = false;
+//线程1:
+context = loadContext();  
+inited = true;  
+//上面两行代码如果不用volatile修饰，可能会发生指令重排，导致报错
+ 
+//线程2:
+while(!inited ){
+sleep()
+}
+doSomethingwithconfig(context);
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="happens-before-规则" tabindex="-1"><a class="header-anchor" href="#happens-before-规则" aria-hidden="true">#</a> Happens-Before 规则</h2><p>“Happens-Before” 是 Java 内存模型中定义的一个重要概念，用于描述多线程环境中内存操作的顺序和可见性。在多线程编程中，Happens-Before 规则的关键在于确保并发操作的顺序性和可见性。同时，Happens-Before 约束了编译器的优化行为，虽允许编译器优化，但是要求编译器优化后一定遵守 Happens-Before 规则。</p><p>具体来说，Happens-Before 规则规定了以下几种情况，需要程序员重点掌握：</p><ol><li><p>程序次序规则：一个线程内的每一个操作，Happens-Before 于该线程内的后续操作。</p></li><li><p>监视器锁规则：对一个锁的解锁操作，Happens-Before 于随后对这个锁的加锁操作。</p></li><li><p>volatile 变量规则：对一个 volatile 变量的写操作，Happens-Before 于随后对这个变量的读操作。</p></li><li><p>传递性：如果 A Happens-Before B，且 B Happens-Before C，则 A Happens-Before C。</p></li></ol><p>除了之前提到的基本规则之外，Happens-Before 还涉及到一些具体的应用场景和含义：</p><ol><li><p>线程启动规则：一个线程 A 启动另一个线程 B，A 线程启动操作的 Happens-Before 关系于 B 线程的任意操作。</p></li><li><p>线程中断规则：对线程 interrupt() 方法的调用 Happens-Before 于被中断线程中的任意操作。</p></li><li><p>终结器规则：对象的构造函数执行 Happens-Before 于该对象的 finalize() 方法。</p></li><li><p>线程终结规则：所有的线程中的操作 Happens-Before 于该线程的终结。</p></li></ol><h2 id="参考资料" tabindex="-1"><a class="header-anchor" href="#参考资料" aria-hidden="true">#</a> 参考资料</h2><p>《Java 并发编程实战》</p><p>《深入理解JVM》</p>`,58),o={href:"http://www.cnblogs.com/dolphin0520/p/3920373.html",target:"_blank",rel:"noopener noreferrer"},p=e("figure",null,[e("img",{src:"https://static-1254191423.cos.ap-shanghai.myqcloud.com/img/common/wxcode.png",alt:"方志朋_官方公众号",tabindex:"0",loading:"lazy"}),e("figcaption",null,"方志朋_官方公众号")],-1);function u(h,b){const n=l("ExternalLinkIcon");return d(),s("div",null,[c,e("p",null,[i("海子的博客："),e("a",o,[i("http://www.cnblogs.com/dolphin0520/p/3920373.html"),r(n)])]),p])}const x=a(v,[["render",u],["__file","2023-12-09-volatile.html.vue"]]);export{x as default};
